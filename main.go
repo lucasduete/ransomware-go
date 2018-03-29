d014a1f122bea33d598a17d5245b1b7743c10568
Lucas Duete <lm.duete@gmail.com>
2018-03-29 02:28:07 -0300

    Recovering Arguments and Enviroment Variables of Operational System and User for define how run the algorithm.

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main()  {
-	
+import (
+	"sync"
+	"flag"
+	"runtime"
+	"os"
+	"fmt"
+)
+
+var (
+	wg            sync.WaitGroup
+	decrypt       bool
+	secret        string
+	directoryBase string
+)
+
+func init() {
+	flag.BoolVar(&decrypt, "decrypt", false, "-decrypt")
+	flag.StringVar(&secret, "secret", "", "-secret=your_secret")
+	flag.Parse()
+
+	if runtime.GOOS == "windows" {
+		directoryBase = "C:/"
+	} else {
+		directoryBase = "/home/" + os.Getenv("USER")
+	}
+}
+
+func main() {
+	fmt.Println(false)
+	fmt.Println(decrypt)
+	fmt.Println(secret)
+	fmt.Println(directoryBase)
 }
