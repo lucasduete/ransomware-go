28223c7f58fc3485214d38378b60d9b57d0150b7
Lucas Duete <lm.duete@gmail.com>
2018-03-29 02:42:02 -0300

    Add new Flag for get what is the path target, if empty verify what is the Operational Sistem, case windows will use 'C:/', case linux will use '/home/lucasduete/'

--- main.go
+++ main.go
@@ -5,31 +5,34 @@ import (
 	"flag"
 	"runtime"
 	"os"
-	"fmt"
 )
 
 var (
 	wg            sync.WaitGroup
 	decrypt       bool
 	secret        string
+	target        string
 	directoryBase string
 )
 
 func init() {
-	flag.BoolVar(&decrypt, "decrypt", false, "-decrypt")
-	flag.StringVar(&secret, "secret", "", "-secret=your_secret")
+	flag.BoolVar(&decrypt, "decrypt", false, "--decrypt")
+	flag.StringVar(&secret, "secret", "", "--secret=your_secret")
+	flag.StringVar(&target, "target", "", "--target=your_path_target")
 	flag.Parse()
 
-	if runtime.GOOS == "windows" {
-		directoryBase = "C:/"
-	} else {
-		directoryBase = "/home/" + os.Getenv("USER")
+	if len(target) == 0 {
+		if runtime.GOOS == "windows" {
+			directoryBase = "C:/"
+		} else {
+			directoryBase = "/home/" + os.Getenv("USER")
+		}
 	}
 }
 
 func main() {
-	fmt.Println(false)
-	fmt.Println(decrypt)
-	fmt.Println(secret)
-	fmt.Println(directoryBase)
+
+	if len(secret) == 0 {
+		panic("Invalid secret, try: --secret=your_secret")
+	}
 }
