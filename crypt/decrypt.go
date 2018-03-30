a8e5a4231abfeb05c40431b0936773d0496e5c7a
Lucas Duete <lm.duete@gmail.com>
2018-03-29 22:09:30 -0300

    Implementation of Class to Decrypt files using Base64

--- crypt/decrypt.go
+++ crypt/decrypt.go
@@ -1,5 +1,30 @@
 package crypt
 
-func Decrypt()  {
-	
+import (
+	"encoding/base64"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+)
+
+func Decrypt(cryptoText []byte, secretKey string) string {
+	key := []byte(secretKey)
+	ciphertext, _ := base64.URLEncoding.DecodeString(string(cryptoText))
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
 }
