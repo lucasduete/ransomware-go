ba5201fb0d0d45144500f0d9d8e28b6bc130cc6f
Lucas Duete <lm.duete@gmail.com>
2018-03-29 22:08:26 -0300

    Implementation of Class to Encrypt files using Base64

--- crypt/encrypt.go
+++ crypt/encrypt.go
@@ -1,5 +1,29 @@
 package crypt
 
-func Encrypt() {
+import (
+	"crypto/aes"
+	"io"
+	"crypto/cipher"
+	"encoding/base64"
+	"crypto/rand"
+)
 
+func Encrypt(file []byte, secretKey string) string {
+	key := []byte(secretKey)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(file))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], file)
+
+	return base64.URLEncoding.EncodeToString(ciphertext)
 }
